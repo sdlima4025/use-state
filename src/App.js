44efa4cbfb75a4f07d47eec1907588a925a7a52b
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-06 20:07:10 -0300

    hook usestate sistema busca CEP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [endereco, setEndereco] =useState({})
+
+  const [enderecos, setEnderecos] = useState([])
+
+  function manipularEndereco (evento) {
+
+    const cep = evento.target.value
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8) {
+      // obter cep
+
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => resposta.json())
+      .then(dados => {
+
+        setEnderecos(lista => [...lista, endereco]) //spread ex: ...lista
+
+
+        setEndereco(enderecoAntigo => {
+          return{
+            ...enderecoAntigo,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf,
+            gia: dados.gia,
+            siafi: dados.siafi
+          }
+        })
+      })
+    }
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input placeholder='Digite o CEP' onChange={manipularEndereco}>
+      </input>
+      <ul>
+        <li>CEP: {endereco.cep}</li>
+        <li>Bairro: {endereco.bairro}</li>
+        <li>Cidade: {endereco.cidade}</li>
+        <li>Estado: {endereco.estado}</li>
+        <li>Gia: {endereco.gia}</li>
+        <li>Siafi: {endereco.siafi}</li>
+      </ul>
       </header>
     </div>
   );
